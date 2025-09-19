970138fedf22e8c050e6822778bd05d21c07a450
savad <124030718+S4vad@users.noreply.github.com>
2025-09-19 20:24:50 +0530

    Change axios base URL to use environment variable

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import axios from "axios";
 import { UserProvider } from "./context/UserContext.jsx";
 import { AppRouter } from "./routes/AppRouter.jsx";
 
-axios.defaults.baseURL = "http://localhost:3000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById("root")).render(
